2cc60f9fe4a0f6ed19ccf1de97aaeaf212edb0ce
github username <github email address>
2018-06-11 22:33:41 -0500

    Add Navigation

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,7 +6,18 @@ import { startLogout } from '../actions/auth'
 export class Header extends React.Component {
   renderLinks = () => {
     if (this.props.authenticated) {
-      
+      return (
+        <div>
+          <button className="button button--link" onClick={this.props.startLogout}>Logout</button>
+        </div>
+      )
+    } else {
+      return (
+        <div>
+          <Link className="button button--link" to="/login">Log In</Link>
+          <Link className="button button--link" to="/signup">Sign Up</Link>
+        </div>
+      )
     }
   }
   render() {
@@ -17,8 +28,9 @@ export class Header extends React.Component {
             <Link className="header__title" to="/dashboard">
               <h1>Oh My Fitness</h1>
             </Link>
-            <Link className="button button--link" to="/signup">Sign Up</Link>
-            <button className="button button--link" onClick={startLogout}>Logout</button>
+            <div className="header__link-right">
+              {this.renderLinks()}
+            </div>
           </div>
         </div>
       </header>
